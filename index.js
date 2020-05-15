02b6094cd4d07c5a8bffb68deddb53421ab5caa6
Wladimir Chópite <wladimir@settlenetwork.com>
2020-05-15 18:18:48 -0300

    [new change]

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const HOST = "0.0.0.0";
 // App
 const app = express();
 app.get("/", (req, res) => {
-  res.send("Hello World!!! dsfsdf");
+  res.send("Hello World From Kubernetes!!!");
 });
 
 app.listen(PORT, HOST);
